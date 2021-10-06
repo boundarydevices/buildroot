f0b5cc4daf1c4030a3edc7ff40e4bccad4dd46c3
Chris Dimich <chris.dimich@boundarydevices.com>
2021-10-06 13:41:32 -0700

    package/freescale-imx/basler-camera: add package
    - binaries required for the Basler daA3840 camera [1]
    - matching Yocto recipe from release hardknott-5.10.35-2.0.0 [2]
    - moved binaries to /usr/share instead of /opt as we did for imx-gpu-viv
    examples
    
    [1]
    https://www.baslerweb.com/en/products/cameras/area-scan-cameras/dart/daa3840-30mc-s-mount/
    [2]
    https://source.codeaurora.org/external/imx/meta-imx/tree/meta-bsp/recipes-bsp/isp-imx/basler-camera_4.2.2.13.0.bb?h=hardknott-5.10.35-2.0.0
    
    Signed-off-by: Chris Dimich <chris.dimich@boundarydevices.com>
    [Gary: moved to /usr/share + update commit log]
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ package/freescale-imx/basler-camera/basler-camera.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# basler-camera
+#
+################################################################################
+
+BASLER_CAMERA_VERSION = 4.2.2.13.0
+BASLER_CAMERA_SITE = $(FREESCALE_IMX_SITE)
+BASLER_CAMERA_SOURCE = basler-camera-$(BASLER_CAMERA_VERSION).bin
+BASLER_CAMERA_DEPENDENCIES = linux
+
+BASLER_CAMERA_LICENSE = NXP Semiconductor Software License Agreement
+BASLER_CAMERA_LICENSE_FILES = EULA COPYING
+BASLER_CAMERA_REDISTRIBUTE = NO
+
+define BASLER_CAMERA_EXTRACT_CMDS
+	$(call FREESCALE_IMX_EXTRACT_HELPER,$(BASLER_CAMERA_DL_DIR)/$(BASLER_CAMERA_SOURCE))
+endef
+
+define BASLER_CAMERA_INSTALL_TARGET_CMDS
+	cp -r $(@D)/opt/* $(TARGET_DIR)/usr/share/
+	cp -r $(@D)/usr/* $(TARGET_DIR)/usr/
+endef
+
+$(eval $(generic-package))
